59246907464f9662c2cf067cab84250a6c1da366
Fabricio Veronez <fabricioveronez@gmail.com>
2024-01-31 12:47:10 -0300

    Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_version = "> 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.89.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "tf-live-storage"
+    storage_account_name = "tflivestorage"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "rg_video" {
+  name     = "aula-rg"
+  location = "East US 2"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s_video" {
+  name                = "aula-aks"
+  location            = azurerm_resource_group.rg_video.location
+  resource_group_name = azurerm_resource_group.rg_video.name
+  dns_prefix          = "aulaaks"
+
+  default_node_pool {
+    name                        = "default"
+    node_count                  = 5
+    vm_size                     = "Standard_D2_v2"
+    temporary_name_for_rotation = "tmppool01"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
