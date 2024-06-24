4a8f8ff2a1814763531bf6089763009a0c4eb22d
Fabricio Veronez <fabricioveronez@gmail.com>
2024-06-24 11:43:16 -0300

    Pipe para a live

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,6 @@ terraform {
       version = "3.89.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "tf-live-storage"
-    storage_account_name = "tflivestorage"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
@@ -32,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "k8s_video" {
 
   default_node_pool {
     name                        = "default"
-    node_count                  = 5
+    node_count                  = 3
     vm_size                     = "Standard_D2_v2"
     temporary_name_for_rotation = "tmppool01"
   }
@@ -40,4 +34,9 @@ resource "azurerm_kubernetes_cluster" "k8s_video" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+resource "local_file" "kubeconfig" {
+  filename = "${path.module}/kubeconfig.yaml"
+  content  = azurerm_kubernetes_cluster.k8s_video.kube_config_raw
 }
